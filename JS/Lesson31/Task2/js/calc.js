7a465ee2b42996a2f853b738cd763652b5b88f25
RomanChorniy <rblackua@gmail.com>
2020-05-08 17:48:51 +0300

    JS. Slick Slider

--- JS/Lesson31/Task2/js/calc.js
+++ JS/Lesson31/Task2/js/calc.js
@@ -1,74 +1,97 @@
-$(document).ready(function () {
-    let $keys = $('button');
-    let $total = $('#tablo');
-    let decimal = false;
-    let operators = ['+', '-', '×', '÷'];
-    $total.addClass("complete");
-
-
-    $keys.click(function () {
-        let keyVal = $(this).data('val');
-        output = $total.html();
-        let lastChar = output[output.length - 1];
-
-        // clear
-        if (keyVal == 'clear') {
-            $total.html('0');
-            decimal = false;
-        }
-
-        // equal
-        else if (keyVal == '=') {
-            if (operators.indexOf(lastChar) > -1 || lastChar == '.')
-                output = output.replace(/.$/, '');
-            if (output) {
-                $total.html(Math.round(eval(output) * 10000000) / 10000000);
+// ------------ my version --------
+$(function () {
+    console.log('jquery ok');
+    let text = '';
+    $('button').click(function (e) {
+        switch (true) {
+            case $(e.target).text() === 'C': { //Clear
+                text = '0';
+                $('#tablo').text(text);
+                break
+            };
+            case true: {
+                console.log($(e.target).text());
+                let tablo = $('#tablo');
+                text += $(e.target).text();
+                tablo.text(text)
+                console.log(tablo.text());
             }
-            $total.addClass("complete");
-            decimal = false;
-        }
-
-        // operators
-        else if ($(this).parent().is('.operation')) {
-            if ($total.is(".complete")) {
-                $total.removeClass("complete");
-            }
-            if (output != '' && operators.indexOf(lastChar) == -1) {
-                $total.html($total.html() + keyVal);
-            } else if (output == '' && keyVal == '-') {
-                $total.html($total.html() + keyVal);
-            }
-            if (operators.indexOf(lastChar) > -1 && output.length > 1) {
-                $total.html($total.html().replace(/.$/, keyVal));
-            }
-            decimal = false;
-        }
-
-        // decimal
-        else if (keyVal == '.') {
-            if ($total.is(".complete")) {
-                $total.html('0' + keyVal);
-                $total.removeClass("complete");
-            } else if (output == '') {
-                $total.html('0' + keyVal);
-            } else if (operators.indexOf(lastChar) > -1) {
-                $total.html($total.html() + '0' + keyVal);
-            } else {
-                if (!decimal) {
-                    $total.html($total.html() + keyVal);
-                    decimal = true;
-                }
-            }
-        }
-
-        // numerals
-        else {
-            if ($total.is(".complete")) {
-                $total.html(keyVal);
-                $total.removeClass("complete");
-            } else {
-                $total.html($total.html() + keyVal);
-            }
-        }
-    })
+        };
+    });
 });
+// _______________________________
+
+// $(document).ready(function () {
+//     let $keys = $('button');
+//     let $total = $('#tablo');
+//     let decimal = false;
+//     let operators = ['+', '-', '×', '÷'];
+//     $total.addClass("complete");
+
+
+//     $keys.click(function () {
+//         let keyVal = $(this).data('val');
+//         output = $total.html();
+//         let lastChar = output[output.length - 1];
+
+//         // clear
+//         if (keyVal == 'clear') {
+//             $total.html('0');
+//             decimal = false;
+//         }
+
+//         // equal
+//         else if (keyVal == '=') {
+//             if (operators.indexOf(lastChar) > -1 || lastChar == '.')
+//                 output = output.replace(/.$/, '');
+//             if (output) {
+//                 $total.html(Math.round(eval(output) * 10000000) / 10000000);
+//             }
+//             $total.addClass("complete");
+//             decimal = false;
+//         }
+
+//         // operators
+//         else if ($(this).parent().is('.operation')) {
+//             if ($total.is(".complete")) {
+//                 $total.removeClass("complete");
+//             }
+//             if (output != '' && operators.indexOf(lastChar) == -1) {
+//                 $total.html($total.html() + keyVal);
+//             } else if (output == '' && keyVal == '-') {
+//                 $total.html($total.html() + keyVal);
+//             }
+//             if (operators.indexOf(lastChar) > -1 && output.length > 1) {
+//                 $total.html($total.html().replace(/.$/, keyVal));
+//             }
+//             decimal = false;
+//         }
+
+//         // decimal
+//         else if (keyVal == '.') {
+//             if ($total.is(".complete")) {
+//                 $total.html('0' + keyVal);
+//                 $total.removeClass("complete");
+//             } else if (output == '') {
+//                 $total.html('0' + keyVal);
+//             } else if (operators.indexOf(lastChar) > -1) {
+//                 $total.html($total.html() + '0' + keyVal);
+//             } else {
+//                 if (!decimal) {
+//                     $total.html($total.html() + keyVal);
+//                     decimal = true;
+//                 }
+//             }
+//         }
+
+//         // numerals
+//         else {
+//             if ($total.is(".complete")) {
+//                 $total.html(keyVal);
+//                 $total.removeClass("complete");
+//             } else {
+//                 $total.html($total.html() + keyVal);
+//             }
+//         }
+//     })
+// });
