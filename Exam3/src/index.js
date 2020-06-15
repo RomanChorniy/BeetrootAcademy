a3dd8e7281fc2deb4a451f842edb4d6e647d45a7
RomanChorniy <rblackua@gmail.com>
2020-06-15 17:23:31 +0300

    add easy scroll to exam3

--- Exam3/src/index.js
+++ Exam3/src/index.js
@@ -3,3 +3,4 @@ import './js/paralax';
 import './js/circleSlider';
 import './js/slick';
 import './js/menu';
+import './js/easyScroll';
--- Exam3/webpack.config.js
+++ Exam3/webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
+const easyScroll = require('easy-scroll');
 
 module.exports = {
     context: path.resolve(__dirname, 'src'),
