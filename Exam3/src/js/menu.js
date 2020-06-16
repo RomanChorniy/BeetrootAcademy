709d62ec23ed20ce14b7305ae20e04153369dab6
RomanChorniy <rblackua@gmail.com>
2020-06-16 16:34:34 +0300

    chenge menu  botton

--- Exam3/src/js/menu.js
+++ Exam3/src/js/menu.js
@@ -5,37 +5,39 @@ $(document).ready(() => {
     let windowHeight = $(window).height();
     $(document).scroll(() => {
         let currentScrollPos = $(document).scrollTop()
-        if (currentScrollPos > lastScrollPos) {
-            $('.logo').css('display', 'none');
-            lastScrollPos = currentScrollPos;
-        }
-        else {
-            $('.logo').css('display', 'flex');
-            lastScrollPos = currentScrollPos;
+        // if (currentScrollPos > lastScrollPos) {
+        //     $('.logo').css('display', 'none');
+        //     lastScrollPos = currentScrollPos;
+        // }
+        // else {
+        //     $('.logo').css('display', 'flex');
+        //     lastScrollPos = currentScrollPos;
+        $('.logo__image').css('display', 'none')
+        $('#vacationChecklist').each(function () {
+            var self = $(this),
+                height = self.offset().top + self.height();
+            if (($(document).scrollTop() + windowHeight >= height) && ($(document).scrollTop() + windowHeight < height + 2451)) {
+                $('.btn__line').addClass('line_black');
+                $('.logo__image').addClass('black_logo');
+            }
+            else {
+                $('.btn__line').removeClass('line_black');
+                $('.logo__image').removeClass('black_logo');
+            }
+        });
 
-            $('#vacationChecklist').each(function () {
-                var self = $(this),
-                    height = self.offset().top + self.height();
-                if (($(document).scrollTop() + windowHeight >= height) && ($(document).scrollTop() + windowHeight < height + 2451)) {
-                    $('.btn__line').addClass('line_black');
-                    $('.logo__image').addClass('black_logo');
-                }
-                else {
-                    $('.btn__line').removeClass('line_black');
-                    $('.logo__image').removeClass('black_logo');
-                }
-            });
-
-        }
+        // }
     });
     $('.menu__btn').click(() => {
         if (!($('.logo').hasClass('menu__open'))) {
             $('.logo').addClass('menu__open');
             $('.menu__page').css('transform', 'translateY(100%)');
+            $('.logo__image').css('display', 'block')
         }
         else {
             $('.logo').removeClass('menu__open');
             $('.menu__page').css('transform', 'translateY(-100%)');
+            $('.logo__image').css('display', 'none')
         }
     });
     $('.menuExit').click(() => {
